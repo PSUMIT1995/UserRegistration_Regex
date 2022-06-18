15c6a5195cc8a4d9937014093d4fe3c3f1b23eee
PSUMIT1995 <psumit191.sp@gmail.com>
2022-06-18 20:01:30 +0530

    UC1 : Validating FirstName

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexValidation regexValidation = new RegexValidation();//Create object
+            Console.WriteLine("Enter Your First Name::-");//take input from the user
+            string FirstName = Console.ReadLine();//Store the input for firstname
+            Console.WriteLine(regexValidation.ValidateFirstName(FirstName));
+        }
+    }
+}
